f79c107f8494d32ceacb3bfbd9e61add51d0a584
Bllose <574931094@qq.com>
2023-03-28 19:28:07 +0800

    管理经济学分组记录

--- sources/grouping/balance.py
+++ sources/grouping/balance.py
@@ -584,6 +584,6 @@ if __name__ == '__main__':
     od.set_path(os.path.abspath('../../resources')) \
         .set_file_name(r'HIT22VCteam.xlsx') \
         .set_sheet_name(r'handler')
-    od.config().set_key(r'B').set_name(r'C').set_gender(r'D').set_datas(r'E-I')
+    od.config().set_key(r'B').set_name(r'C').set_gender(r'D').set_datas(r'E-K')
     grouping = Grouping(od.load())
-    grouping.process(order=7).showTheLastGroup().showTheLastOrder()
+    grouping.process(order=6).showTheLastGroup().showTheLastOrder()
