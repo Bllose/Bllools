ff69e66206c1b7659129dc8e94273c7adde09b10
Bllose <bllose2018@gmail.com>
2025-01-26 13:34:00 +0800

    restart

--- sources/helper/config/config_type.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-class type(Enum):
-    SYS = 0
-    ENV = 1
-    FILE = 2
-    
--- sources/test/logTest/lib/mylib.py
+++
@@ -1,8 +0,0 @@
-# mylib.py
-import logging
-logger = logging.getLogger(__name__)
-
-def do_something():
-    logger.info('Doing something')
-    logger.setLevel(level=10)
-    logger.debug('a debug info')
